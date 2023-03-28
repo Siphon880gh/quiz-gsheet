b5ae2b0c1d67f4a9205b9692e8295b1fc1e8ffec
Weng Fei Fung <weffung@ucdavis.edu>
2023-03-27 22:32:30 -0700

    Added meta description

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ for($i=0; $i<count($gsheetPaths); $i++) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="By Weng Fei Fung. Take various quizzes for learning purposes. Currently music sight reading lessons. Coming soon - ICU topics.">
     <title>Quiz</title>
 
     <!-- Styling  -->
